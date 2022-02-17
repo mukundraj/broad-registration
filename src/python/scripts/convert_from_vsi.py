27b1cc80aa84ec2561d7b89a3a669e02207d65ba
Mukund Raj <mraj@broadinstitute.org>
2022-02-17 15:09:37 -0500

    Updating output filename in vsi conversion script
    Makes is easier to load in to slicer

--- src/python/scripts/convert_from_vsi.py
+++ src/python/scripts/convert_from_vsi.py
@@ -9,7 +9,12 @@ Created by Mukund on 2022-02-16
 
 Usage: python script.py path_to_input_folder path_to_output_folder opfileformat
 
-Usage example: python src/python/scripts/convert_from_vsi.py /Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/vsi /Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/tif_from_vsi png
+Usage example: 
+
+python src/python/scripts/convert_from_vsi.py \
+/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/vsi \
+/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/png_from_vsi \
+png
 
 References:
 - https://docs.openmicroscopy.org/bio-formats/5.7.3/users/comlinetools/conversion.html
@@ -34,7 +39,7 @@ files = [f for f in onlyfiles if os.path.splitext(f)[-1]==".vsi"]
 for file in files:
     fullpath = input_path+"/"+file
     print(fullpath)
-    fullpath_op = output_path+"/"+file
+    fullpath_op = output_path+"/nis_"+file
     pre, ext = os.path.splitext(fullpath_op)
     fullpath_op = pre+"."+opfileformat
     print(fullpath_op)
