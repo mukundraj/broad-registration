9b5a9805fab32a5422ab5450435b1d821b22c388
Mukund Raj <mraj@broadinstitute.org>
2022-02-18 09:06:24 -0500

    Updating convert to gray script to accept both tif and png and commandline inputs

--- src/python/scripts/convert_to_gray_png.py
+++ src/python/scripts/convert_to_gray_png.py
@@ -1,14 +1,29 @@
+"""
+
+Script to read in png/tiff files in a folder and convert to grayscale pngs
+
+Usage example:
+
+python ./src/python/scripts/convert_to_gray_png.py \
+/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/tif_8x_downsampled \
+/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/tif_8x_downsampled_gray
+
+"""
 import os
 from os import listdir
 from os.path import isfile, join
 import shutil
 import subprocess
+import sys
 
-parent = "/Users/mraj/Desktop/work/data/mouse_atlas/initial_converted_subset"
-parent = "/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/tif_8x_downsampled"
+
+# parent = "/Users/mraj/Desktop/work/data/mouse_atlas/initial_converted_subset"
+# parent = "/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/tif_8x_downsampled"
+parent = sys.argv[1]
 
 files_to_copy = []
-path_to_copy_to = "/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/tif_8x_downsampled_gray"
+# path_to_copy_to = "/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/tif_8x_downsampled_gray"
+path_to_copy_to = sys.argv[2]
 
 # for i in range(1,91):
 #     path =parent+"/"+str(i)
@@ -27,14 +42,15 @@ path_to_copy_to = "/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_
 path = parent
 
 onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-files = [ fi for fi in onlyfiles if fi.endswith(".tif") ]
-for i in range(3):
+files = [ fi for fi in onlyfiles if fi.endswith(".tif") or fi.endswith(".png") ]
+for i in range(len(files)):
     filename =parent+'/'+files[i]
-    print (path)
+    print (filename)
     base = os.path.splitext(filename)[0]
-    base = base.replace("downsampled", "downsampled_gray")
-    png_filename = base+".png"
-    print(base)
+    # basefilename = base.replace("downsampled", "downsampled_gray")
+    basefilename = os.path.basename(base)
+    png_filename = path_to_copy_to+"/"+basefilename+".png"
+    print(png_filename)
     # if (len(files)==1):
     #     filename  = parent+'/'+str(i)+'/'+files[0]
     #     files_to_copy.append(filename)
