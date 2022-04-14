6a1e6b0a8a347b5f9d1b8d4f0dd6786b75a5dd71
Mukund Raj <mraj@broadinstitute.org>
2022-04-14 12:51:07 -0400

    Updated arguments to handle babylon coords (and any possibly other) as well

--- src/python/scripts/misc/symlink_creator.py
+++ src/python/scripts/misc/symlink_creator.py
@@ -5,13 +5,15 @@ Usage:
 
 python symlink_creator.py \
     symlink start path \
-    symlink end path
+    symlink end path \
+    filename prefix
 
 Usage example:
 
 python src/python/scripts/misc/symlink_creator.py \
     /Users/mraj/Desktop/work/projects/active/broad-registration/src/viewer2/data \
-    /Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/s6_register_to_allen/s6_allen_coords
+    /Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/s6_register_to_allen/s6_babylon_coords \
+    babylon_img_coords
 
 
 Created by Mukund on 2022-04-10
@@ -22,17 +24,16 @@ import sys
 import numpy as np
 import subprocess
 
-
-
 nissl_ids = [i for i in np.arange(1,228,2)]
 
 source_path = sys.argv[1]
 target_path = sys.argv[2]
+filename_prefix = sys.argv[3]
 
 for nis_id in nissl_ids:
     nis_id_str = str(nis_id).zfill(3)
-    source = f'./allen_img_coords_{nis_id_str}.csv'
-    target = f'{target_path}/allen_img_coords_{nis_id_str}.csv'
+    source = f'./{filename_prefix}_{nis_id_str}.csv'
+    target = f'{target_path}/{filename_prefix}_{nis_id_str}.csv'
     print(source)
     subprocess.run(["ln", "-s", target, source], cwd=source_path)
 
