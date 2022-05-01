0e6bd79f5903bdcf2c53639ac652d317e5f6ed25
Mukund Raj <mraj@broadinstitute.org>
2022-05-01 17:07:10 -0400

    Stub with useful reference for nonlinear transformation of image using fiducials

---
+++ src/cpp/img_mapper.cpp
@@ -0,0 +1,14 @@
+// References 
+// - https://itk.org/ITKExamples/src/Filtering/ImageGrid/ResampleAnImage/Documentation.html
+
+#ifndef IMG_MAPPER_CPP
+#define IMG_MAPPER_CPP
+
+int main(){
+
+
+  return 0;
+}
+
+
+#endif /* IMG_MAPPER_CPP */
