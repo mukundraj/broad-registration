deb34ca57052713d98fee96af29c2e2f035f1b75
Mukund Raj <mraj@broadinstitute.org>
2022-05-17 09:12:43 -0400

    Added debug statements for figuring out non leaf node ids appearing in nrrd files

--- src/python/scripts/allensdk/allen_sdk_test.py
+++ src/python/scripts/allensdk/allen_sdk_test.py
@@ -17,41 +17,73 @@ References:
 import os
 from allensdk.core.reference_space_cache import ReferenceSpaceCache
 import nrrd
+from produtils import dprint
+import numpy as np
 
 reference_space_key = 'annotation/ccf_2017/'
 resolution = 25
+# resolution = 10
 rspc = ReferenceSpaceCache(resolution, reference_space_key, manifest='manifest.json')
+# rspc = ReferenceSpaceCache(resolution, reference_space_key)
 # ID 1 is the adult mouse structure graph
 tree = rspc.get_structure_tree(structure_graph_id=1)
 
-
-# annotation, meta = rspc.get_annotation_volume()
+annotation, meta = rspc.get_annotation_volume()
 
 # # The file should appear in the reference space key directory
 # os.listdir(reference_space_key)
 #
 
-print(os.listdir(reference_space_key))
+dprint(os.listdir(reference_space_key))
 
-print(tree.get_structures_by_name(['Cerebrum']))
+# dprint(tree.get_structures_by_name(['Cerebrum', 'Hippocampal formation']))
 cerebrum_node=tree.get_structures_by_name(['Cerebrum'])
+hippo_form_node = tree.get_structures_by_name(['Hippocampal formation'])
+induseum_node = tree.get_structures_by_name(['Induseum griseum'])
+ammons_node = tree.get_structures_by_name(['Ammon\'s horn'])
+hippo_region_node = tree.get_structures_by_name(['Hippocampal region'])
+
+dprint(induseum_node)
+print("")
+print("")
+dprint(hippo_form_node)
+print("")
+print("")
+dprint(ammons_node)
+print("")
+print("")
+dprint(hippo_region_node)
 basic_groups_node=tree.get_structures_by_name(['Basic cell groups and regions'])
 
 nrrd_path = f'{reference_space_key}/annotation_25.nrrd'
 readdata, header = nrrd.read(nrrd_path)
-print(readdata.shape)
-print(header)
+dprint(readdata.shape)
+dprint(header)
 
-print (readdata[268, 160, 228])
+unique, counts = np.unique(readdata, return_counts=True)
+count_dict = dict(zip(unique, counts))
+dprint(len(count_dict.keys()))
+dprint(count_dict[1089])
+dprint(count_dict[19])
+
+dprint(hippo_region_node[0]['id'])
+tofind = hippo_region_node[0]['id']
+if (tofind in readdata):
+    dprint("found it", tofind)
+else:
+    dprint("not found")
+
+
+dprint (readdata[268, 160, 228])
 
 id = readdata[268, 160, 228]
 
 name_map = tree.get_name_map()
-print(id, name_map[id])
-print(id, cerebrum_node[0]['id'])
+dprint(id, name_map[id])
+dprint(id, cerebrum_node[0]['id'])
 
-print(tree.structure_descends_from(id, cerebrum_node[0]['id']))
-print(tree.structure_descends_from(id, basic_groups_node[0]['id']))
+dprint(tree.structure_descends_from(id, cerebrum_node[0]['id']))
+dprint(tree.structure_descends_from(id, basic_groups_node[0]['id']))
 # labels = [readdata[pt[0]][pt[1]] for pt in nissl_img_corrds]
 #
 
--- src/python/scripts/allensdk/get_anno_vol.py
+++ src/python/scripts/allensdk/get_anno_vol.py
@@ -3,6 +3,10 @@ Script to get Allen SDK annotation volume
 
 Created by Mukund on 2022-02-28
 
+Usage example:
+
+python src/python/scripts/allensdk/get_anno_vol.py
+
 References:
 - https://allensdk.readthedocs.io/en/latest/reference_space.html
 - https://allensdk.readthedocs.io/en/latest/_static/examples/nb/reference_space.html#Downloading-an-annotation-volume
@@ -14,6 +18,7 @@ from allensdk.core.reference_space_cache import ReferenceSpaceCache
 
 reference_space_key = 'annotation/ccf_2017'
 resolution = 25
+resolution = 10
 rspc = ReferenceSpaceCache(resolution, reference_space_key, manifest='manifest.json')
 # ID 1 is the adult mouse structure graph
 tree = rspc.get_structure_tree(structure_graph_id=1) 
