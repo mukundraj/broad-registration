54568b81142a377d054ecd1e9dbbcc0150b2f259
Mukund Raj <mraj@broadinstitute.org>
2022-06-13 09:47:52 -0400

    R matrix package based aggregation for heatmap

---
+++ src/python/scripts/analysis/s9c_qc_main.py
@@ -0,0 +1,147 @@
+"""
+
+Usage:
+
+Usage example:
+
+python src/python/scripts/analysis/s9c_qc_main.py \
+    /Users/mraj/Desktop/work/data/mouse_atlas \
+    /data_v3_nissl_post_qc/s7_annotations/bead_ccf_labels_allbds \
+    /Users/mraj/Desktop/work/data/temp_data/2022-05-04/integrated_mats \
+    /temp \
+    /data_v3_nissl_post_qc/s9_analysis/qc_mats/processed \
+    1 7 \
+    True \
+    /data_v3_nissl_post_qc/s9_analysis/qc_mats/processed
+
+Created by Mukund on 2022-06-11
+
+"""
+
+import sys
+import subprocess
+from produtils import dprint
+from allensdk.core.reference_space_cache import ReferenceSpaceCache
+import os
+import json
+import numpy as np
+
+data_root = sys.argv[1]
+ip_folder_labels = data_root+sys.argv[2]
+ip_folder_counts = sys.argv[3]
+op_folder_tmp = data_root+sys.argv[4]
+op_folder = data_root+sys.argv[5]
+start_pid = sys.argv[6]
+end_pid = sys.argv[7]
+nonzero = sys.argv[8]=='True'
+op_folder = sys.argv[1]+sys.argv[9]
+
+
+regions_names = [
+   [ "Primary somatosensory area, trunk, layer 1"],
+   [ "Primary somatosensory area, trunk, layer 2/3"],
+   [ "Primary somatosensory area, trunk, layer 4"],
+   [ "Primary somatosensory area, trunk, layer 5"],
+   [ "Primary somatosensory area, trunk, layer 6a"],
+   [ "Primary somatosensory area, trunk, layer 6b"],
+   [ "Hippocampal formation"]]
+
+reference_space_key = 'annotation/ccf_2017'
+resolution = 25
+rspc = ReferenceSpaceCache(resolution, reference_space_key, manifest='manifest.json')
+# ID 1 is the adult mouse structure graph
+tree = rspc.get_structure_tree(structure_graph_id=1) 
+
+regions_ids = []
+for layer_names in regions_names:
+    dprint(layer_names)
+    # dprint(tree.get_structures_by_name(layer_names))
+    regions_ids.append(list(map(lambda x: x['id'], tree.get_structures_by_name(layer_names))))
+
+dprint(regions_ids)
+
+regions_ids_str = ''
+for layer in regions_ids:
+    for id in layer:
+        regions_ids_str+=str(id)
+    regions_ids_str+=';'
+regions_ids_str = regions_ids_str[:-1]
+dprint(regions_ids_str)
+
+# genes_list = ['Dcn', 'Ndnf']
+genes_list = ['Ndnf']
+# region_ids = '234,234,234;33,33,32;'
+
+# subprocess.run(["fd", "^.*tif$" , "-x", "convert", "{}", "-alpha", "off", "{}" ], cwd=op_path)
+
+
+# iterate over genes_list
+
+for gene in genes_list:
+
+    # call R script
+    r_op_file_name = f'{op_folder_tmp}/{gene}.csv'
+    dprint(ip_folder_labels)
+    dprint(ip_folder_counts)
+    dprint(r_op_file_name)
+    subprocess.run(["Rscript", "src/python/scripts/analysis/s9c_qc_prep.R", \
+                    gene, ip_folder_labels, ip_folder_counts, r_op_file_name, \
+                    regions_ids_str, \
+                    start_pid, end_pid, \
+                    str(nonzero)])
+
+    # read gene matrix and prepare dict object
+    data_z = np.genfromtxt(r_op_file_name, delimiter=',')
+    n_regions,n_pucks = np.shape(data_z)
+    # dprint(data_z.tolist())
+    dprint(n_regions,n_pucks)
+    data_dict = {"x": list(range(1, n_pucks+1)), "y": list(range(1, n_regions+1)), "z":data_z.tolist()}
+
+    # write out gene data json
+    fname_val = "fnz" if nonzero else "fumi"
+    fname = f'{fname_val}.json'
+    op_gene_data_file = f'{op_folder}/{fname}'
+    dprint(nonzero, fname_val)
+    with open(op_gene_data_file, 'w') as outfile:
+        json.dump(data_dict, outfile)
+
+    # prepare metadata
+    desc_val = "nonzero count" if nonzero else "count"
+    metadata_dict = {"name":f'{gene}',
+                     "desc":f'gene {desc_val} normalized by nUMI in region',
+                     "filename":f'{fname}'}
+
+    dprint(metadata_dict)
+
+    # update metadata file
+    metadata_path =  f'{op_folder}/metadata.json'
+    file_exists = os.path.exists(metadata_path)
+
+    meta_key_val = 'ana_metadata_nonzero' if nonzero else 'analysis_metadata'
+    if (file_exists):
+        with open(metadata_path, 'r') as f:
+            metadata = json.load(f)
+        dprint(metadata[meta_key_val])
+        idxs = list(i for i, x in enumerate(metadata[meta_key_val]) if x['name'] == 'Ndnf')
+        if (len(idxs)>0):
+            metadata[meta_key_val][idxs[0]] = metadata_dict
+        else:
+            metadata[meta_key_val].append(metadata_dict)
+    else:
+        metadata = {
+            "analysis_metadata": [],
+            "ana_metadata_nonzero": []
+        }
+        metadata[meta_key_val].append(metadata_dict)
+
+    # write out metadata
+    with open(metadata_path, 'w') as outfile:
+        json.dump(metadata, outfile)
+
+    # copy folder to gcp storage
+    # gsutil -m cp -r /Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/s9_analysis/qc_mats/processed gs://ml_portal2/test_data2/qc_mats/
+
+# https://github.com/conda/conda/issues/9589
+# https://discuss.pytorch.org/t/intel-mkl-fatal-error-cannot-load-libmkl-core-dylib/67710/3s
+
+
---
+++ src/python/scripts/analysis/s9c_qc_prep.R
@@ -0,0 +1,171 @@
+#!/usr/bin/env Rscript
+#
+# Reads gene and output nRegion x nPuck matrix for genes specified in argument
+#
+# Usage example:
+#
+#  Rscript src/python/scripts/analysis/s9c_qc_prep.R
+#
+# Created by Mukund on 2022-06-11
+
+library(anndata)
+Sys.setenv(RETICULATE_PYTHON = "/Users/mraj/opt/miniconda3/envs/Rv4/bin/python3")
+library(reticulate)
+library(Matrix)
+library(Matrix.utils)
+
+
+
+args = commandArgs(trailingOnly=TRUE)
+print(args)
+# 
+# gene = 'Ndnf'
+# ip_folder_labels <- "/Users/mraj/Desktop/work/data/mouse_atlas/data_v3_nissl_post_qc/s7_annotations/bead_ccf_labels_allbds"
+# ip_folder_counts <- "/Users/mraj/Desktop/work/data/temp_data/2022-05-04/integrated_mats"
+# r_op_filename <- "/Users/mraj/Desktop/work/data/mouse_atlas/temp/Ndnf.csv"
+# region_ids <- '1089'
+# start_pid <- 79
+# end_pid <- 83
+# nonzero <- FALSE
+# region_ids <- '1006;670;1086;1111;9;461;1089'
+
+gene <- args[1]
+ip_folder_labels <- args[2]
+ip_folder_counts <- args[3]
+r_op_filename <- args[4]
+region_ids <- args[5]
+start_pid <- as.integer(args[6])
+end_pid <- as.integer(args[7])
+nonzero <- args[8]=='True'
+print(start_pid)
+print(end_pid)
+print(region_ids)
+print(nonzero)
+
+br_layer_ids <- strsplit(region_ids,";")
+
+rids_lists <- vector(mode = "list", length = length(br_layer_ids[[1]]))
+ii=0
+for (brlstr in br_layer_ids[[1]]){
+  ii=ii+1
+  
+  brl_ids <- strsplit(brlstr[[1]], ',')
+  rids_lists[[ii]] <- vector(mode = "list", length = length(brl_ids[[1]]))
+  # brl_ids <- as.numeric(unlist(brl_ids))
+  # brl_ids <- unlist(brl_ids)
+
+  jj=0
+  for (brl_id in brl_ids){
+        jj=jj+1
+        
+        rids_lists[[ii]][[jj]] <- brl_id
+  }
+}
+
+pucks <- seq(start_pid,end_pid,2)
+gene_mat <- matrix(0, nrow = length(rids_lists), ncol = length(pucks))
+print(pucks)
+# loop over pucks
+pid_idx <- 0
+for (pid in pucks) {
+  print(pid)
+  pid_idx <- pid_idx + 1
+  apid = pid
+  if (pid==5 || pid==77 || pid==167)
+    apid = pid - 2 ## adjusted pid todo: modify viewer to not require this adjustment
+   
+
+
+  # get data matrix of puck
+  ip_counts_file = paste0(ip_folder_counts, "/ad_counts_", apid, ".h5ad")
+  print(ip_counts_file)
+  ad <- read_h5ad(ip_counts_file)
+  X <- t(ad$X)
+  print(dim(X))
+  print(length(ad$obs_names))
+  # filter selected gene
+  gene_idx <- ad$obs_names==gene
+  X_filt <- X[,gene_idx]
+  totals_umis <- rowSums(X) # total umis f
+  total_umis_gene <- totals_umis[gene_idx]
+  #print(length(totals_umis))
+  print(c('len X_filt', length(X_filt)))
+  
+  
+  
+  # get labels factor
+  ip_labels_file = paste0(ip_folder_labels, '/allen_anno_data_',sprintf("%03d", apid),'.csv')
+  print(ip_labels_file)
+  labels <- factor(read.csv(file=ip_labels_file, header=FALSE)$V10)
+  
+  # aggregate over regions
+  b<-aggregate.Matrix(X_filt,labels, FUN=sum())
+  
+  cmat<-aggregate.Matrix(X,labels, FUN=sum())
+  #print(b)
+  print('entering loop')
+  # populate heatmap column for this puck by iterating over required regions
+  for (ii in seq(length(rids_lists))){ # looping over region
+   
+    sum <- 0;
+    for (jj in seq(length(rids_lists[[ii]]))){ ## looping over sub region
+      
+      # print("jj")
+      # print(rids_lists[ii][jj][[1]][[1]]);
+      # print(levels(labels))
+      # print(any(levels(labels)==rids_lists[ii][jj][[1]][[1]]))
+      # print(any(levels(labels)=="1089"))
+      
+      
+      # print('total_region_umis')
+      # print(total_region_umis)
+      
+      # get gene count in sub region
+      if (any(levels(labels)==rids_lists[ii][jj][[1]][[1]])){
+        total_region_umis <- sum(cmat[rids_lists[ii][jj][[1]][[1]],])
+        cnt <- b[rids_lists[ii][jj][[1]][[1]], , drop = FALSE]@x
+        if (length(cnt)==1){
+          # update count in mat
+          if (nonzero){
+            gene_mat[ii, pid_idx] <- gene_mat[ii, pid_idx]+1
+          }else{
+            gene_mat[ii, pid_idx] <- gene_mat[ii, pid_idx]+cnt/total_region_umis
+            #print(c(ii,jj,rids_lists[[ii]][[jj]][[1]][[1]]))
+          }
+          
+        }
+       
+      }
+    }
+  }
+  
+  gc()
+  
+}
+
+# write out output matrix csv
+write.table(gene_mat, file=r_op_filename, row.names=FALSE, col.name=FALSE, sep=',')
+
+# matrix(, nrow = 2, ncol = 2)
+# any(levels(labels)=="1016")
+# b['1016', , drop = FALSE]@x
+# write.table(mm, file='temp.csv', row.names=FALSE, col.name=FALSE, sep=',')
+# 
+# https://stat.ethz.ch/pipermail/r-help/2011-April/275773.html
+# r_op_filename = '/Users/mraj/Desktop/work/data/mouse_atlas/temp/Ndnf-cnt.csv'
+# gene_mat <- as.matrix(read.table(file=r_op_filename, sep=','))
+# colnames(gene_mat) = pucks
+# rownames(gene_mat) = c('L1', 'L2/3','L4', 'L5', 'L6', 'L7', 'Hippo Formation' )
+# heatmap(gene_mat, Colv = NA, Rowv = NA, scale="column")
+
+# #Initial Heatmap 
+# install.packages("tidyverse")
+# library(tidyverse)
+# gene_mat %>% 
+#   as.data.frame() %>%
+#   rownames_to_column("f_id") %>%
+#   pivot_longer(-c(f_id), names_to = "samples", values_to = "counts") %>%
+#   mutate(samples= fct_relevel(samples,colnames(dat))) %>%
+#   ggplot(aes(x=samples, y=f_id, fill=counts)) + 
+#   geom_raster() + 
+#   scale_fill_viridis_c()
