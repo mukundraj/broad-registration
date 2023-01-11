2bf6fbf5f72b42ee533c8ddd04e75af618629377
Mukund Raj <mraj@broadinstitute.org>
2023-01-11 11:51:51 -0500

    Change CellSpatial tab's selection fields
    New fields are percent of celltype in chosen region and celltype count
    in chosen region

--- src/python/scripts/analysis_cs/s2d_region_enrich.py
+++ src/python/scripts/analysis_cs/s2d_region_enrich.py
@@ -63,24 +63,26 @@ annotation, meta = rspc.get_annotation_volume()
 tree_nodes_info = []
 
 # for hydrating tree nodes and populating an array of tree node data
-def get_child_info(tree, child_id, name_map, parent_id, nGenes, rid_to_idx_local, dataXcsr, bead_counts):
+def get_child_info(tree, child_id, name_map, parent_id, nCelltypes, rid_to_idx_local, dataXcsr, bead_counts):
 
     children = tree.children(child_id)
-    data = {"rid":child_id, "nz_counts":np.zeros(nGenes), "num_beads":0}
+    data = {"rid":child_id, "nz_counts":np.zeros(nCelltypes), "num_beads":0, "nz_counts_nn":np.zeros(nCelltypes)}
 
     if (len(children)>0):
         if child_id in rid_to_idx_local:
             local_row_idx = rid_to_idx_local[child_id]
             if bead_counts[local_row_idx][1] > 0:
                 data['nz_counts'] += np.squeeze(dataXcsr.getrow(local_row_idx).toarray())
+                data['nz_counts_nn'] += np.squeeze(dataXcsr.getrow(local_row_idx).toarray())
                 data['num_beads'] += bead_counts[local_row_idx][1]
         for cur_child_node in children:
             cur_child_id = cur_child_node.identifier
-            info = get_child_info(tree, cur_child_id, name_map, child_id, nGenes, rid_to_idx_local, dataXcsr, bead_counts)
+            info = get_child_info(tree, cur_child_id, name_map, child_id, nCelltypes, rid_to_idx_local, dataXcsr, bead_counts)
 
             # assimalate data into parent
             data['nz_counts'] += info['nz_counts']
             data['num_beads'] += info['num_beads']
+            data['nz_counts_nn'] += info['nz_counts_nn']
 
 
     else: # leaf node - read data for region and update counts
@@ -88,12 +90,13 @@ def get_child_info(tree, child_id, name_map, parent_id, nGenes, rid_to_idx_local
         if child_id in rid_to_idx_local:
             local_row_idx = rid_to_idx_local[child_id]
             data['nz_counts'] += np.squeeze(dataXcsr.getrow(local_row_idx).toarray())
+            data['nz_counts_nn'] += np.squeeze(dataXcsr.getrow(local_row_idx).toarray())
             data['num_beads'] += bead_counts[local_row_idx][1]
 
     tree_nodes_info.append(data)
     return data
 
-# for getting nGenes
+# for getting nCelltypes
 ip_data_file1 = ip_gth_aggr_data_folder+"/nz_aggr_scores_003.h5ad"
 dprint(ip_data_file1)
 data1 = ann.read_h5ad(ip_data_file1)
@@ -101,8 +104,8 @@ data1X = data1.X
 data1Xcsr = csr_matrix(data1X)
 # dprint(data1Xcsr)
 # dprint(data1Xcsr.getrow(0))
-_, nGenes = data1Xcsr.shape
-# dprint("nGenes", nGenes)
+_, nCelltypes = data1Xcsr.shape
+# dprint("nCelltypes", nCelltypes)
 # read total count file for mapping info from local index to rid
 
 
@@ -129,6 +132,9 @@ curPuckSums = None
 maxExpr = None
 maxExprPuck = None
 
+colsums = np.zeros(nCelltypes)
+
+# pids = [1,41]
 for pid in pids:
     dprint("pid: ", pid)
     assert(pid!=5 and pid!=77 and pid!=167)
@@ -143,7 +149,20 @@ for pid in pids:
     dataXcsr = csr_matrix(dataX)
     M, N = dataXcsr.shape
     aggr_counts_file = f'{ip_gth_aggr_data_folder}/nz_aggr_num_beads_{nis_id_str}.csv'
-    all_region_nz_array = np.squeeze(np.array(dataXcsr[1,:].sum(axis=0))) # excluding region zero (non tissue)
+    # all_region_nz_array = np.squeeze(np.array(dataXcsr[1,:].sum(axis=0))) # excluding region zero (non tissue)
+
+    dprint(M,N)
+
+    colsums += np.squeeze(np.array(dataXcsr.sum(axis=0)))
+    # dataXcsrNorm = dataXcsr/colsums # normalize by total numbeads expressing > threshold for each celltype
+    # dataXcsrNorm = csr_matrix(dataXcsrNorm)
+
+    # if (pid==41):
+
+
+    #     dprint('bmat', np.squeeze(dataXcsrNorm.getrow(55).toarray())[1548])
+    #     dprint('bmat', np.squeeze(dataXcsrNorm.getcol(1548).toarray()))
+    #     dprint('sum', np.sum(np.squeeze(dataXcsrNorm.getcol(1548).toarray())))
 
     bead_counts = np.genfromtxt(aggr_counts_file, delimiter=',', skip_header=1).astype(np.int32)
     m, n = np.shape(bead_counts)
@@ -161,13 +180,15 @@ for pid in pids:
         dsh.add_nodes(tree, nodes_list, name_map)
 
     tree_nodes_info = []
-    data = get_child_info(tree, ancestors_list[0][0], name_map, "", nGenes, rid_to_idx_local, dataXcsr, bead_counts)
+    data = get_child_info(tree, ancestors_list[0][0], name_map, "", nCelltypes, rid_to_idx_local, dataXcsr, bead_counts)
     dprint("final tree_nodes_info ", tree_nodes_info[-1])
     rid_to_idx_map = {}
     rids = []
     for idx, x in enumerate(tree_nodes_info):
         rid_to_idx_map[x['rid']]=idx
         rids.append(x['rid'])
+        # if (x['rid']==961 or x['rid']==165):
+        #     dprint("x['rid']", x['rid'], idx)
     nRids = len(tree_nodes_info)
     if pid==1:
         rids = np.array(rids)
@@ -178,51 +199,47 @@ for pid in pids:
 
         # for storing global (as opposed to puckwise) info
         globalGroup = root.create_group(f'pall', overwrite=True)
-        globalGroupX = globalGroup.zeros('X', shape=(nRids, nGenes), chunks=(1, nGenes), dtype='f4')
+        globalGroupX = globalGroup.zeros('X', shape=(nRids, nCelltypes), chunks=(1, nCelltypes), dtype='f4')
         globalGroupX[:] = 0
-        globalGroupXout = globalGroup.zeros('Xout', shape=(nRids, nGenes), chunks=(1, nGenes), dtype='f4')
+        globalGroupXout = globalGroup.zeros('Xout', shape=(nRids, nCelltypes), chunks=(1, nCelltypes), dtype='f4')
         # globalGroupXout = np.tile(all_region_nz_array, (nRids, 1))  # initializing here to prevent multiple inclusion of exterior beads
-        gzVal = np.zeros(nRids)
-        gzValOut = np.zeros(nRids)
+        gzVal = np.zeros((nRids, nCelltypes))
+        # gzValOut = np.zeros((nRids, nCelltypes))
 
-        maxExpr = np.zeros(nGenes)
-        maxExprPuck = np.ones(nGenes)
+        maxExpr = np.zeros(nCelltypes)
+        maxExprPuck = np.ones(nCelltypes)
 
 
-    pgroupX = np.zeros((nRids, nGenes))
-    pgroupXout = np.zeros((nRids, nGenes))
+    curGlobalGroupX = np.zeros((nRids, nCelltypes))
+    # pgroupXout = np.zeros((nRids, nCelltypes))
 
-    all_region_beads = np.sum(np.array([x['num_beads'] for x in tree_nodes_info]), axis=0)
+    # all_region_beads = np.sum(np.array([x['num_beads'] for x in tree_nodes_info]), axis=0)
 
+    # dprint(dataXcsr.shape)
+    # dprint(dataXcsr.sum(axis=0).shape)
+    # dprint(dataXcsr.sum(axis=0))
+    # exit(0)
+
+    # for current puck populating global region idx map and accumulating gzVal for each region
     for rid in rid_to_idx_map: 
         global_region_idx = rid_to_idx_map[rid]
-        # dprint(rid_to_idx_map)
-        # dprint('rid', rid, 'global_region_idx', global_region_idx)
-        # dprint(gzVal)
 
-        zval = tree_nodes_info[global_region_idx]['num_beads']
-        gzVal[global_region_idx] += zval
+        # if rid present in current puck update gzVal
+        if (rid in rid_to_idx_local):
+            localRid = rid_to_idx_local[rid]
+            gzVal[global_region_idx,:] += np.sum(np.squeeze(dataXcsr.getrow(localRid).toarray()))
+            # gzValOut[global_region_idx,:] += np.sum(gzVal[global_region_idx,:])
 
+
+        # if (rid==961):
+        #     dprint('rid961', np.amax(tree_nodes_info[global_region_idx]['nz_counts']))
         # pgroupX[global_region_idx,:] = tree_nodes_info[global_region_idx]['nz_counts']
         globalGroupX[global_region_idx,:] += tree_nodes_info[global_region_idx]['nz_counts'] # will be normalized later
-        pgroupX[global_region_idx,:] = tree_nodes_info[global_region_idx]['nz_counts']/zval # normalized her to determine puck with max expr per 10k beads
-
-        # now calculate % nonzero outsize region
-        zvalOut = tree_nodes_info[rid_to_idx_map[997]]['num_beads'] - tree_nodes_info[global_region_idx]['num_beads'] # denomminator -> num of beads outside current region
-        gzValOut[global_region_idx] += zvalOut
-        # if (gzValOut[global_region_idx] > 0):
-        #     dprint(gzValOut[global_region_idx])
-
-        # pgroupXout[global_region_idx,:] = all_region_nz_array - tree_nodes_info[global_region_idx]['nz_counts'] # numerator -> num of nonzero count beads outside current region
-        pgroupXout[global_region_idx,:] = tree_nodes_info[rid_to_idx_map[997]]['nz_counts'] - tree_nodes_info[global_region_idx]['nz_counts'] # numerator -> num of nonzero count beads outside current region, will be normalized later
-        globalGroupXout[global_region_idx,:] += pgroupXout[global_region_idx,:]
-        pgroupXout[global_region_idx,:] /= zvalOut # normalized here to maintain consistency with pgroupX
-
-    # dprint("DEBUG", pid, globalGroupX[1098, 8198], gzVal[1098], globalGroupXout[1098, 8198], gzValOut[1098])
-    # dprint("DEBUG", pid, tree_nodes_info[1098]['nz_counts'][8198] , gzVal[1098], pgroupXout[1098, 8198], gzValOut[1098])
+        globalGroupXout[global_region_idx,:] += tree_nodes_info[global_region_idx]['nz_counts_nn'] # will be normalized later
+        curGlobalGroupX[global_region_idx,:] = tree_nodes_info[global_region_idx]['nz_counts'] # for determining puck with max cell counts
     sys.stdout.flush()
 
-    curPuckSums = np.sum(np.nan_to_num(pgroupX[:,:]), axis=0) # summmation over regions of regionwise normalized expression counts for all genes in current puck
+    curPuckSums = np.sum(np.nan_to_num(curGlobalGroupX[:,:]), axis=0) 
     # dprint('pgroupX ', np.nan_to_num(pgroupX[:]))
     # dprint('curPuckSums', curPuckSums)
     # dprint(' max curPuckSums', np.max(curPuckSums))
@@ -231,37 +248,39 @@ for pid in pids:
 
     maxExpr[curGreaterIdxs] = curPuckSums[curGreaterIdxs]
     maxExprPuck[curGreaterIdxs] = pid
+    dprint('maxExprPuck SerEx', maxExprPuck[936], maxExpr[936], curPuckSums[936])
 
     pgroup_genes = root.create_group('genes', overwrite=True)
     # pgroup_genes = zarr.empty(5, dtype=object, object_codec=numcodecs.JSON())
-    # pgroup_genes_arr = root.zeros('genes', shape=(nGenes), dtype='S6')
-    # pgroup_genes = root.zeros('X', shape=(1,nGenes), dtype='S6')
-    pgroup_genes_arr = pgroup_genes.zeros('X', shape=(nGenes), dtype='object', object_codec=numcodecs.JSON())
+    # pgroup_genes_arr = root.zeros('genes', shape=(nCelltypes), dtype='S6')
+    # pgroup_genes = root.zeros('X', shape=(1,nCelltypes), dtype='S6')
+    pgroup_genes_arr = pgroup_genes.zeros('X', shape=(nCelltypes), dtype='object', object_codec=numcodecs.JSON())
     # pgroup_genes_arr[0, :] = np.asarray(gene_names)
     pgroup_genes_arr[:] = np.asarray(celltype_names)
 
     if pid==np.max(pids):
         dprint(np.max(globalGroupX), np.max(globalGroupXout)) #  max of global nz beads in and out of regions
         dprint(np.min(globalGroupX), np.min(globalGroupXout)) #  min of global nz beads in and out of regions
-        dprint(np.max(gzVal), np.max(gzValOut)) # max of global all beads in and out of all regions
-        dprint('all_region_nz_array max', np.max(all_region_nz_array), tree_nodes_info[rid_to_idx_map[997]]['num_beads'])
-        # normalizing
-        globalGroupX[:] = np.transpose(np.nan_to_num(np.transpose(globalGroupX)/gzVal, posinf=0))[:,:] # posinf=0: if denominator (total beads) is zero, then expression must be zero too
+        # dprint(np.max(gzVal), np.max(gzValOut)) # max of global all beads in and out of all regions
+        # dprint('all_region_nz_array max', np.max(all_region_nz_array), tree_nodes_info[rid_to_idx_map[997]]['num_beads'])
 
-        globalGroupXout[:] = np.transpose(np.nan_to_num(np.transpose(globalGroupXout)/gzValOut, posinf=0))[:,:] # posinf=0: if denominator (total beads) is zero, then expression must be zero too
+        temp = np.transpose(np.nan_to_num(np.transpose(globalGroupX)/np.transpose(gzVal), posinf=0))[:,:]
+
+        dprint('shape', np.shape(temp))
+
+        # normalizing
+        # globalGroupX[:] = np.transpose(np.nan_to_num(np.transpose(globalGroupX)/np.transpose(gzVal), posinf=0))[:,:] # posinf=0: if denominator (total beads) is zero, then expression must be zero too
+        globalGroupX[:] = np.transpose(np.nan_to_num(np.transpose(globalGroupX/colsums), posinf=0))[:,:] # posinf=0: if denominator (total beads) is zero, then expression must be zero too
+        globalGroupXout[:] = np.transpose(np.nan_to_num(np.transpose(globalGroupXout), posinf=0))[:,:] # posinf=0: if denominator (total beads) is zero, then expression must be zero too
+        dprint('temp', globalGroupX[414, 1548]) # 414 is global reg idx of piriform area(allenid 961), 1547 is celltype ExSlc30a3_Ssmem1_1, 1548 is for Ex_Rorb_Ptpn20
+        dprint('temp', globalGroupXout[414, 1548]) # 414 is global reg idx of piriform area(allenid 961), 1547 is celltype ExSlc30a3_Ssmem1_1, 1548 is for Ex_Rorb_Ptpn20
+        # globalGroupXout[:] = np.transpose(np.nan_to_num(np.transpose(globalGroupXout)/gzValOut, posinf=0))[:,:] # posinf=0: if denominator (total beads) is zero, then expression must be zero too
 
         dprint(np.max(globalGroupX), np.max(globalGroupXout))
 
-        # storing normalizing denominators
-        gzValX = globalGroup.zeros('gzVal', shape=(nRids), chunks=(nRids), dtype='f4')
-        gzValX[:] = gzVal[:]
-
-        gzValOutX= globalGroup.zeros('gzValOut', shape=(nRids), chunks=(nRids), dtype='f4')
-        gzValOutX[:] = gzValOut[:]
-
         # write out meta info
         celltype_names_dict = {"data": list(celltype_names), "maxExprPuck": list(maxExprPuck)}
-        dprint(celltype_names)
+        # dprint(celltype_names)
         # Serializing json
         json_object = json.dumps(celltype_names_dict, indent=4)
 
