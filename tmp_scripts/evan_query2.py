13b2ab070c6489a757806c901a3e2348c3449ce4
Mukund Raj <1138446+mukundraj@users.noreply.github.com>
2023-12-18 10:40:12 -0500

    Update celltype list based on latest data and query
    Less celltypes since '' types removed

--- tmp_scripts/evan_query2.py
+++ tmp_scripts/evan_query2.py
@@ -1,5 +1,9 @@
 """Create a csv of all celltypes and available metadata for each celltype.
 
+Updates: 
+    - 2023-12-17: new cell list based based on new scZarr file. Also removing
+      additionalMetadata column.
+
 phthon evan_query2.py
     ip: data_root
     ip: scZarr path relative to data_root
@@ -9,8 +13,8 @@ Example:
 
 python tmp_scripts/evan_query2.py \
     ~/Desktop/work/data/mouse_atlas \
-    /single_cell/s1/scZarr_321017.zarr \
-    /misc/for_evan/cluster_metadata.csv \
+    /single_cell/s1/scZarr_231207.zarr \
+    /misc/for_evan/celltypes_list.csv \
 
 Created by Mukund on 2023-10-18
 """
@@ -39,7 +43,7 @@ dprint(z.tree())
 cellclusters = z.obs['clusters']
 cellclusters = [x.split('=')[1] for x in cellclusters]
 
-additionalMetadata = z.metadata['additionalMetadata']
+# additionalMetadata = z.metadata['additionalMetadata']
 
 
 # dprint(cellclusters[:])
@@ -48,9 +52,9 @@ additionalMetadata = z.metadata['additionalMetadata']
 # write out csv with columns cellclusters and additionalMetadata
 
 with open(out_file, 'w') as f:
-    f.write('cellclusters,additionalMetadata\n')
+    f.write('celltype,status\n')
     for i in range(len(cellclusters)):
-        f.write(cellclusters[i] + ',' + additionalMetadata[i] + '\n')
+        f.write(cellclusters[i] + ',''\n')
 
 
 
